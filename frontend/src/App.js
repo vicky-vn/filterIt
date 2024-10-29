2f89f5a5b5e8022e25e0fafd06bbf576cc21186b
imprakashraghu <prakashjaw@hotmail.com>
2024-10-29 12:12:11 -0400

    tested

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './pages/Home';
 import Header from './components/Header';
-// import Process from './pages/Process';
+import Process from './pages/Process';
 import Settings from './pages/Settings';
 import About from './pages/About';
 
@@ -12,7 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path='/' Component={Home} />
-        {/* <Route path='/process' Component={Process} /> */}
+        <Route path='/process' Component={Process} />
         <Route path='/settings' Component={Settings} />
         <Route path='/about' Component={About} />
       </Routes>
