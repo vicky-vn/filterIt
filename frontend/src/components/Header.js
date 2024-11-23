1226082af3caa911c3a12186fa73fd9336137dcb
imprakashraghu <prakashjaw@hotmail.com>
2024-11-23 10:01:51 -0500

    done

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -13,11 +13,13 @@ function Header() {
   return (
     <div className='w-full px-10 py-6 text-md flex flex-row items-center justify-between'>
         <div className='flex items-center space-x-2'>
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="bg-white rounded-md w-5 h-5 text-red-500">
-                <path fillRule="evenodd" d="M12.516 2.17a.75.75 0 0 0-1.032 0 11.209 11.209 0 0 1-7.877 3.08.75.75 0 0 0-.722.515A12.74 12.74 0 0 0 2.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 0 0 .374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 0 0-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08Zm3.094 8.016a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
-            </svg>
+            <img 
+                src='./logo.png'
+                alt='logo'
+                className='contain h-[30px]'
+            />
             <Link to='/'>
-            <h1 className='tracking-tighter text-lg text-left text-black font-bold'>filterIt</h1>
+            <h1 className='tracking-tighter text-xl text-left text-black font-bold'>filterIt</h1>
             </Link>
         </div>
         <div className='grid grid-cols-4 gap-4'>
